785d0bbd85fcfba16718faf453b9be64f4d5d9e8
exctac <exctac@yandex.ru>
2017-06-14 23:13:20 +0300

    begin article app

--- article/views.py
+++ article/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from article.models import Article
 
-# Create your views here.
+
+class ArticleDetail(DetailView):
+    template_name = "article.html"
+    model = Article
+    context_object_name = 'article'
+    slug_field = 'alias'
+    slug_url_kwarg = 'alias'
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetail, self).get_context_data()
+        return context
--- vedansha/urls.py
+++ vedansha/urls.py
@@ -4,15 +4,18 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from homepage.views import HomePage
+from article.views import ArticleDetail
 
 urlpatterns = [
     # Urls for Admin
     url(r'^admin/', admin.site.urls),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     # url(r'^jet/', include('jet.urls', 'jet')),
     # url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+
     # Urls for Site
     url(r'^$', HomePage.as_view(), name='home'),
-    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^company/(?P<alias>)$', ArticleDetail.as_view(), name='company'),
 ]
 
 if settings.DEBUG:
